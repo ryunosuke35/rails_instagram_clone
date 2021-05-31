b303b0d4d60a46caaa32c0ec4269ef552e87f97c
ryunosuke35 <6bev2103@gmail.com>
2021-06-01 01:55:24 +0900

    画像アップロード機能の実装

--- Gemfile
+++ Gemfile
@@ -3,6 +3,10 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.5'
 
+gem 'carrierwave'
+gem 'mini_magick'
+gem 'mimemagic', '0.3.7'
+
 gem 'rails', '~> 5.2.5'
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 3.11'
@@ -26,6 +30,9 @@ group :development do
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'pry-rails'
+  gem 'pry-byebug'
+  gem 'meta_request'
 end
 
 group :test do
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_31_144007) do
+ActiveRecord::Schema.define(version: 2021_05_31_160747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pictures", force: :cascade do |t|
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
