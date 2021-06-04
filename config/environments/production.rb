bf2df349eca5a73fb88012951601fa70a05bd86d
ryunosuke35 <6bev2103@gmail.com>
2021-06-05 00:38:54 +0900

    変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,17 +94,16 @@ Rails.application.configure do
 
 
 
-  config.action_mailer.default_url_options = { host: 'hogehoge-fugafuga.herokuapp.com' }
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: "heroku.com",
-    address: "smtp.sendgrid.net",
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  config.action_mailer.default_url_options = { host: 'pure-caverns-99341.herokuapp.com' }
+    ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = {
+      user_name: ENV['SENDGRID_USERNAME'],
+      password: ENV['SENDGRID_PASSWORD'],
+      domain: "heroku.com",
+      address: "smtp.sendgrid.net",
+      port: 587,
+      authentication: :plain,
+      enable_starttls_auto: true
+    }
 
-  
 end
