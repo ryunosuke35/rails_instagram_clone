d65cac416d8745a8371e55bf33c039b8ae3e1b9b
ryunosuke35 <6bev2103@gmail.com>
2021-06-04 13:31:17 +0900

    変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile_picture)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile_picture, :profile_picture_cache)
   end
 end
